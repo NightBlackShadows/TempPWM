e5ddbee10bf160bda4240b08fe0c6902a051a1af
NightBlackShadows <35600687+NightBlackShadows@users.noreply.github.com>
2018-04-07 12:42:14 +0200

    Removed and fixed some code

--- TempPWM.ino
+++ TempPWM.ino
@@ -1,33 +1,22 @@
-/*     ---------------------------------------------------------
- *     |  Arduino Experimentation Kit Example Code             |
- *     |  CIRC-10 .: Temperature :. (TMP36 Temperature Sensor) |
- *     ---------------------------------------------------------
- *   
- *  A simple program to output the current temperature to the IDE's debug window 
- * 
- *  For more details on this circuit: http://tinyurl.com/c89tvd 
- */
-
 //TMP36 Pin Variables
 int temperaturePin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
                         //the resolution is 10 mV / degree centigrade 
                         //(500 mV offset) to make negative temperatures an option
 
 // Initial Fan Speeds
-int frontSpeed = 0;
+int topSpeed = 0;
+
+// Temp
+float temperature = 0;
 
 // PWM Pins
-int front_a = 5;
-int front_b = 6;
+int top_a = 5;
+int top_b = 6;
 
-/*
- * setup() - this function runs once when you turn your Arduino on
- * We initialize the serial connection with the computer
- */
 void setup()
 {
-  pinMode(front_a, OUTPUT);   //front 140mm
-  pinMode(front_b, OUTPUT);   //front 140mm
+  pinMode(top_a, OUTPUT);   //top 140mm
+  pinMode(top_b, OUTPUT);   //top 140mm
   Serial.begin(9600);  //Start the serial connection with the copmuter
                        //to view the result open the serial monitor 
                        //last button beneath the file bar (looks like a box with an antenae)
@@ -37,19 +26,19 @@ void setup()
  
 void loop()                     // run over and over again
 {
- float temperature = getVoltage(temperaturePin);  //getting the voltage reading from the temperature sensor
+ temperature = getVoltage(temperaturePin);  //getting the voltage reading from the temperature sensor
  temperature = (temperature - .5) * 100;          //converting from 10 mv per degree wit 500 mV offset
                                                   //to degrees ((volatge - 500mV) times 100)
 
- frontSpeed = 100 + ((temperature-15)*20);
+ topSpeed = 100 + ((temperature-15)*20);
                                                   
  // Serial output for debugging
- Serial.print("Front: "); Serial.println(frontSpeed);
+ Serial.print("Top: "); Serial.println(topSpeed);
  Serial.println();
 
  // Write the changes to the PWM pins
- analogWrite(front_a, frontSpeed);
- analogWrite(front_b, frontSpeed);
+ analogWrite(top_a, topSpeed);
+ analogWrite(top_b, topSpeed);
                                                   
  Serial.println(temperature);                     //printing the result
  delay(1000);                                     //waiting a second
